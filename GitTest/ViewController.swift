7b76b19b6c0f6544f3e9e3e9f85400d45c158115
Your Name <info@gsquarewebtech.com>
2019-08-11 23:43:11 +0530

    ganesh 2

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("ganesh push nre branch ntesdd tgyhyh")
+        print("ganesh push nre branch ntesdd tgyhyh 6788888")
     }
     
 }
